a3caa6ac32475e2279c2e16d40314c633eb8d853
Grodis Kostas <grodiscostas@hotmail.com>
2017-07-22 10:19:21 +0000

    Add links to nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  get '/home',  to: 'static_pages#home'
 
-  get 'static_pages/help'
+  get '/help',  to: 'static_pages#help'
 
-  get 'static_pages/about'
+  get '/about',  to: 'static_pages#about'
 
   root 'static_pages#home'
 end
